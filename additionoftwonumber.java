c4d84db41e1b58209e973671d72a0b9aa6b9cc07
Your Name <pravinsonvar7722>
2023-01-03 20:10:30 +0530

    addition two number programm

---
+++ additionoftwonumber.java
@@ -0,0 +1,131 @@
+
+public class additionoftwonumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Govind");
+				int a = 10; 
+				int b = 20;
+				int c = a + b;
+				System.out.println(c);
+				//System.out.println("adition of two no"+c);
+				//System.out.println("a");
+				//System.out.println("a");
+				//System.out.println("a");
+
+
+	}
+
+}
+*********swaping two number******
+public class fristproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		System.out.println("value of A" +a);
+		System.out.println("value of B" +b);
+		int c = a;
+		    a = b;
+		    b = c;
+			System.out.println("After swap value of A" +a);
+			System.out.println("Before swap value of A" +b);
+
+
+	}
+}
+
+
+
+o/p 
+before a=10
+after b=20 then
+output
+
+value of A10
+value of B20
+After swap value of A20
+Before swap value of A10
+
+    ********or swaping ****
+
+public class fristproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		System.out.println("value of A" +a);
+		System.out.println("value of B" +b);
+		  a =  a * b;
+		    b = a / b;
+		    a = a / b;
+			System.out.println("After swap value of A" +a);
+			System.out.println("Before swap value of A" +b);
+
+
+	}
+     
+}
+***********conditional statment*****
+1) IF ...
+2)IF...ElSE...
+3)IF....ELSE....IF
+4) Nested IF..
+******1) if********
+
+public class fristproject {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		if(20 > 18)
+		{	
+		      System.out.println(" 20 is greater than 18 ");
+		}
+	} 
+    }
+
+
+
+***********OR************
+
+public class fristproject {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int x = 20, y = 18;
+		if(x > y)
+		{	
+		      System.out.println(" x is greater than y ");
+		}
+	} 
+    }
+
+
+**********IF....else....******
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
